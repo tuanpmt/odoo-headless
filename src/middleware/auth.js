ada2458f6b2f4c0b2256fb1b6f68f1c41237512d
Tuan <tuan@Tuans-Mac-mini.local>
2026-02-08 22:31:11 +0700

    refactor: clean up project structure - single server.js + views at root

--- src/middleware/auth.js
+++
@@ -1,9 +0,0 @@
-function requireAuth(req, res, next) {
-  if (req.session && req.session.user) {
-    res.locals.user = req.session.user;
-    return next();
-  }
-  res.redirect('/login');
-}
-
-module.exports = { requireAuth };
